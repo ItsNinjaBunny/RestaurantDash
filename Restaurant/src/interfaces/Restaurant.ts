263427b38d50dec55506d8e1a076d28940e1efed
Brayden Wong <braydenlwong@gmail.com>
2022-08-03 09:09:50 -0600

    updated Restaurant

--- Restaurant/src/interfaces/Restaurant.ts
+++ Restaurant/src/interfaces/Restaurant.ts
@@ -1,7 +1,13 @@
-import item from './item';
+import item from './item'
+import coupon from './Coupon';
 
 export default interface Restaurant {
     _id : number,
     name : string,
-    menu_items : item[]
+    menu_items : item[],
+    coupon : coupon[],
+    location : {
+        formatted_address : string,
+        coords : number[]
+    }
 }
